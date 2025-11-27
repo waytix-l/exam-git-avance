77f199794bdbe13793716462dc993af138d4748e
vboxuser <quentin.mateus@ynov.com>
2025-11-27 10:38:50 +0000

    [FIX]-delete book file and fix all file

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,10 +2,15 @@ package handlers
 
 import (
 	"html/template"
-<<<<<<< HEAD
 	"main/data"
 	"net/http"
+	"strconv"
 )
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	tmpl.Execute(w, books)
+}
 
 var books = []data.Book{
 	{1, "The Go Programming Language", "Alan Donovan", 2015},
@@ -13,48 +18,37 @@ var books = []data.Book{
 	{3, "The Pragmatic Programmer", "Andrew Hunt", 1999},
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
-	tmpl.Execute(w, books)
-=======
-	"net/http"
-	"strconv"
-    "main/data"
-)
+func BookHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
 
-var books = []data.Book{
-    {1, "The Go Programming Language", "Alan Donovan", 2015},
-    {2, "Clean Code", "Robert C. Martin", 2008},
-    {3, "The Pragmatic Programmer", "Andrew Hunt", 1999},
+	if idStr == "" {
+		http.Error(w, "ID requis", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "ID invalide", http.StatusBadRequest)
+		return
+	}
+
+	var selected *data.Book
+	for _, b := range books {
+		if b.ID == id {
+			selected = &b
+		}
+	}
+
+	if selected == nil {
+		http.Error(w, "Livre introuvable", http.StatusNotFound)
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/book.html"))
+	tmpl.Execute(w, selected)
 }
 
-func BookHandler(w http.ResponseWriter, r *http.Request) {
-    idStr := r.URL.Query().Get("id")
-
-    if idStr == "" {
-        http.Error(w, "ID requis", http.StatusBadRequest)
-        return
-    }
-
-    id, err := strconv.Atoi(idStr)
-    if err != nil {
-        http.Error(w, "ID invalide", http.StatusBadRequest)
-        return
-    }
-
-    var selected *data.Book
-    for _, b := range books {
-        if b.ID == id {
-            selected = &b
-        }
-    }
-
-    if selected == nil {
-        http.Error(w, "Livre introuvable", http.StatusNotFound)
-        return
-    }
-
-    tmpl := template.Must(template.ParseFiles("templates/book.html"))
-    tmpl.Execute(w, selected)
->>>>>>> templates/book
+func ContactHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/contact.html"))
+	tmpl.Execute(w, nil)
 }
--- main.go
+++ main.go
@@ -3,22 +3,17 @@ package main
 import (
 	"log"
 	"main/handlers"
-	"main/handlers"
 	"net/http"
 )
 
 func main() {
 	log.Println("Serveur Go en démarrage...")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	http.HandleFunc("/", handlers.HomeHandler)
-=======
+
 	http.HandleFunc("/contact", handlers.ContactHandler)
->>>>>>> templates/contact
-=======
+
 	http.HandleFunc("/book", handlers.BookHandler)
->>>>>>> templates/book
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
